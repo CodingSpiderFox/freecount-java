2f9d6aa397f1819c4b8e1b1048d51d21c5c0e7eb
codingspiderfox <codingspiderfox@gmail.com>
2021-10-02 09:22:43 +0000

    fix: exit from run script

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker-compose -f src/main/docker/keycloak.yml up -d&
 docker-compose -f src/main/docker/elasticsearch.yml up -d&
 docker-compose -f src/main/docker/postgresql.yml up -d&
 sleep 20
-./mvnw
+./mvnw&
